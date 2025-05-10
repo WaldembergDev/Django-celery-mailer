c2022a8e9450d7834ee97317919ffeaf2f5891d7
waldembergdev <waldemberg.pereira@outlook.com>
2025-05-10 16:25:55 -0300

    Criado a view e template registration

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class GuestUser(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import GuestUser
 
 # Create your views here.
+def registration(request):
+    if request.method == 'GET':
+        return render(request, 'registration.html')
+    else:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        user = GuestUser(
+            name = name,
+            email = email
+        )
+        user.save()
+        return redirect('/account/registration')
